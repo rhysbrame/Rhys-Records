bbb1b066fd178d69446b348690b87f40d1d8afe1
Rhys Brame <rhysbrame@gmail.com>
2017-03-31 17:44:49 +0100

    added sql file with tables

---
+++ rhys_records_app/db/rhys_records.sql
@@ -0,0 +1,24 @@
+DROP TABLE genre;
+DROP TABLE albums;
+DROP TABLE artists;
+
+CREATE TABLE artists
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE albums
+(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  quantity INT4,
+  cost_price INT4,
+  artist_id INT8 REFERENCES artists(id)
+);
+
+CREATE TABLE genre
+(
+  id SERIAL8 PRIMARY KEY,
+  type VARCHAR(255)
+);
