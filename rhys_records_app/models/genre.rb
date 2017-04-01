218f9595b334b53e3ee68de912782f5efbf473b1
Rhys Brame <rhysbrame@gmail.com>
2017-04-01 18:18:05 +0100

    added find albums to genre class

--- rhys_records_app/models/genre.rb
+++ rhys_records_app/models/genre.rb
@@ -16,6 +16,12 @@ class Genre
     @id = results.first()['id'].to_i
   end
 
+  def albums()
+    sql = "SELECT * FROM genres g INNER JOIN albums b ON b.genre_id =g.id WHERE g.id = #{@id}"
+    results = SqlRunner.run(sql)
+    return results.map{ |album_by_genre_hash| Album.new(album_by_genre_hash) }
+  end
+
   def self.all()
     sql = "SELECT * FROM genres"
     results = SqlRunner.run(sql)
