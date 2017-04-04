c7edcf2a1f8f161e1b8a92e8e36439abd67a3822
Rhys Brame <rhysbrame@gmail.com>
2017-04-04 19:07:22 +0100

    added get function to album controller

--- rhys_records_app/controllers/albums_controller.rb
+++ rhys_records_app/controllers/albums_controller.rb
@@ -1,7 +1,14 @@
 require_relative('../models/album.rb')
 
+#index
 get '/albums' do
   @albums = Album.all()
   # @genres = Genre.all()
   erb ( :'albums/index' )
 end
+
+#new album
+get '/albums/new' do
+  erb (:'albums/new')
+end
+
