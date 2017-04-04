22de297637441dd10a2e76123fa4a30123663c47
Rhys Brame <rhysbrame@gmail.com>
2017-04-04 18:30:26 +0100

    added a artists in genre

--- rhys_records_app/controllers/genres_controller.rb
+++ rhys_records_app/controllers/genres_controller.rb
@@ -22,3 +22,22 @@ get '/genres/:id' do
   @genre = Genre.find(params[:id])
   erb ( :'genres/show' )
 end
+
+#update genres
+get '/genres/:id/edit' do
+  @genre = Genre.find(params[:id])
+  erb ( :'genres/edit')
+end
+
+post '/genres/:id' do
+  @genre = Genre.new(params)
+  @genre.update
+  erb ( :'genres/show' )
+end
+
+#delete artist
+post '/genres/:id/delete' do
+  @genre = Genre.find(params[:id])
+  @genre.delete()
+  redirect ('/genre')
+end
