fd82abb06bf7b0529ff415e287843eb559b1bb7e
Rhys Brame <rhysbrame@gmail.com>
2017-04-05 20:08:15 +0100

    collated more seeds

--- rhys_records_app/db/seeds.rb
+++ rhys_records_app/db/seeds.rb
@@ -1,7 +1,6 @@
 require_relative('../models/artist')
 require_relative('../models/genre')
 require_relative('../models/album')
-require('pry')
 
 Album.delete_all()
 Genre.delete_all()
@@ -22,6 +21,51 @@ artist4.save()
 artist5 = Artist.new({'name' => 'Bob Dylan'})
 artist5.save()
 
+artist6 = Artist.new({'name' => 'Hank Marvin'})
+artist6.save()
+
+artist7 = Artist.new({'name' => 'Abba'})
+artist7.save()
+
+artist8 = Artist.new({'name' => 'Staves'})
+artist8.save()
+
+artist9 = Artist.new({'name' => 'Dodgy'})
+artist9.save()
+
+artist10 = Artist.new({'name' => 'Fun Lovin Criminals'})
+artist10.save()
+
+artist11 = Artist.new({'name' => 'Gene'})
+artist11.save()
+
+artist12 = Artist.new({'name' => 'Jayhawks'})
+artist12.save()
+
+artist13 = Artist.new({'name' => 'Kula Shaker'})
+artist13.save()
+
+artist14 = Artist.new({'name' => 'Lovin Spoonful'})
+artist14.save()
+
+artist15 = Artist.new({'name' => 'Whiskeytown'})
+artist15.save()
+
+artist16 = Artist.new({'name' => 'Eric Clapton'})
+artist16.save()
+
+artist17 = Artist.new({'name' => 'David Bowie'})
+artist17.save()
+
+artist18 = Artist.new({'name' => 'Doors'})
+artist18.save()
+
+artist19 = Artist.new({'name' => 'Led Zepelin'})
+artist19.save()
+
+artist20 = Artist.new({'name' => 'Modest Mouse'})
+artist20.save()
+
 genre1 = Genre.new({'type' => 'Acoustic'})
 genre1.save()
 
@@ -30,6 +74,27 @@ genre2.save()
 
 genre3 = Genre.new({'type' => 'Rock'})
 genre3.save()
+
+genre4 = Genre.new({'type' => 'Pop'})
+genre4.save()
+
+genre5 = Genre.new({'type' => 'Electro'})
+genre5.save()
+
+genre6 = Genre.new({'type' => 'House'})
+genre6.save()
+
+genre7 = Genre.new({'type' => 'Hip Hop'})
+genre7.save()
+
+genre8 = Genre.new({'type' => 'Emo'})
+genre8.save()
+
+genre9 = Genre.new({'type' => 'Metal'})
+genre9.save()
+
+genre10 = Genre.new({'type' => 'Grunge'})
+genre10.save()
 
 album1 = Album.new({
   'title' => 'Blonde on Blonde',
@@ -85,5 +150,38 @@ album6 = Album.new({
 })
 album6.save()
 
-# binding.pry
-# nil
+album7 = Album.new({
+  'title' => '3',
+  'quantity' => 3,
+  'cost_price' => 150,
+  'artist_id' => artist19.id,
+  'genre_id' => genre3.id
+})
+album7.save()
+
+album8 = Album.new({
+  'title' => 'Ziggy Stardust',
+  'quantity' => 2,
+  'cost_price' => 225,
+  'artist_id' => artist17.id,
+  'genre_id' => genre4.id
+})
+album8.save()
+
+album9 = Album.new({
+  'title' => 'K',
+  'quantity' => 22,
+  'cost_price' => 15,
+  'artist_id' => artist13.id,
+  'genre_id' => genre2.id
+})
+album9.save()
+
+album10 = Album.new({
+  'title' => 'LA Woman',
+  'quantity' => 1,
+  'cost_price' => 400,
+  'artist_id' => artist18.id,
+  'genre_id' => genre3.id
+})
+album10.save()
